34d5fd39e0c566c23e96ca61f595734322a4d5a4
NomfundoNgwenya <Nomfundo Ngwenya@.com>
2023-07-14 15:05:52 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main - Entry point
+ * main - Printing without printf and puts
  *
- * Return: Always 1 (success)
+ * Return: Always (1)
+ *
  */
 int main(void)
 {
-	write(2, "and that place of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
