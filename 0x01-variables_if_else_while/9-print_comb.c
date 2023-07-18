57db329771149de215053ab213f2cdd052509ce9
NomfundoNgwenya <Nomfundo Ngwenya@.com>
2023-07-17 18:04:42 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print all possible combinations of single-digit numbers
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	int numbers = '0';
+
+	while (numbers <= '9')
+	{
+		putchar(numbers);
+		if (numbers <= '8')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		numbers++;
+	}
+	putchar('\n');
+	return (0);
+}
