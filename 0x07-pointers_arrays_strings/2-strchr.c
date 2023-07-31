0e8669ab09ca05e28f95a5820d55512a74f2f698
NomfundoNgwenya <Nomfundo Ngwenya@.com>
2023-07-31 08:06:07 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,21 +2,18 @@
 #include <stddef.h>
 /**
  * _strchr - Locates the first occurrence of a character in a string.
- * @s: Pointer to the string to search.
- * @c: The character to locate.
- *
- * Return: Pointer to the first occurrence of @c in @s, or NULL if not found.
+ * @s: input
+ * @c: input
+ * Return: (0)
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
-	{
-		if (*s == c)
-		{
-			return (s);
-		}
-		s++;
-	}
+	int i = 0;
 
-	return (NULL);
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
 }
