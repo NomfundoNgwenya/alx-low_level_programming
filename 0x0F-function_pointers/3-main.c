5566ccf89317d3bf09442ec8a959f931fecfec10
NomfundoNgwenya <Nomfundo Ngwenya@.com>
2023-08-18 16:32:29 -0700

    100-main_opcodes

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,8 +11,8 @@
  */
 int main(int argc, char *argv[])
 {
-	 char *op; /* operator */
-	 int num1, num2; /* number inputs */
+	char *op; /* operator */
+	int num1, num2; /* number inputs */
 
 	 if (argc != 4) /* if argument count is wrong */
 	 {
@@ -20,27 +20,27 @@ int main(int argc, char *argv[])
 		 exit(98);
 	 }
 
-	 /* set cmd argument input */
-	 num1 = atoi(argv[1]); /* first number input */
-	 op = (argv[2]); /* operator input */
-	 num2 = atoi(argv[3]); /* second number input */
+	  /* set cmd argument input */
+	  num1 = atoi(argv[1]); /* first number input */
+	   op = (argv[2]); /* operator input */
+	   num2 = atoi(argv[3]); /* second number input */
 
-	 /* if operator not in operator list */
-	 if (get_op_func(op) == NULL || op[1] != '\0')
-	 {
-		 printf("Error\n");
-		 exit(99);
-	 }
+	   /* if operator not in operator list */
+	   if (get_op_func(op) == NULL || op[1] != '\0')
+	   {
+		   printf("Error\n");
+		   exit(99);
+	   }
 
-	 /* if user tries to divide or perform modulus by 0 */
-	 if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
-	 {
-		 printf("Error\n");
-		 exit(100);
-	 }
+	   /* if user tries to divide or perform modulus by 0 */
+	    if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+	    {
+		     printf("Error\n");
+		      exit(100);
+	    }
 
-	 /* print results of operation */
-	 printf("%d\n", get_op_func(op)(num1, num2));
+	     /* print results of operation */
+	     printf("%d\n", get_op_func(op)(num1, num2));
 
-	 return (0);
+	      return (0);
 }
