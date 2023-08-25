cfef488cf4cde20a591f9cc06458e6b5a0455376
NomfundoNgwenya <Nomfundo Ngwenya@.com>
2023-08-24 17:30:21 -0700

    Print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,16 +1,25 @@
 #ifndef LIST_H
 #define LISH_H
 
-#include <stdlib.h>
+#include <stddef.h>
 #include <string.h>
 #include <stdio.h>
 
+/**
+ * struct list_s - singly linked list
+ * @len: length of the string
+ * @next: points to the next node
+ * @str: string - (malloc'ed string)
+ *
+ * Description: singly linked list node structure
+ */
 typedef struct list_s
 {
 	char *str;
 	size_t len;
 	struct list_s *next;
 } list_t;
+
 size_t print_list(const list_t *h);
 
-#endif
+#endif /* LIST_H */
