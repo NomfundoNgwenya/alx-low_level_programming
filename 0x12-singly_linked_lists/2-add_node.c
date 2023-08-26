e70a7f7e5f59148746fb6b4004acceff3a942252
NomfundoNgwenya <Nomfundo Ngwenya@.com>
2023-08-25 19:55:23 -0700

    Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,12 +1,31 @@
 #include "lists.h"
-
 /**
  * add_node - Adds a new node of a list_t list.
  * @head: Pointer to the pointer of the linked list.
  * @str: String to be added to the new node.
- *
- *  Return: The address of the new element, or NULL if it failed.
+ * Return: The address of the new element, or NULL if it failed.
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	
+	list_t *new_node;
+	unsigned int length = 0;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return NULL;
+	}
+	new_node->len = strlen(str);
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	new_node->len = length;
+	if (*head != NULL)
+		new_node->next = *head;
+	if (*head == NULL)
+		new_node->next = NULL;
+	*head = new_node;
+	return (*head);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,9 +2,9 @@
 #define LISTS_H
 
 #include <stddef.h>
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
  * struct list_s - singly linked list
  * @len: length of the string
