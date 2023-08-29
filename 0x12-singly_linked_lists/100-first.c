b09112adf0a01b2b66339e70766bc50099684a30
NomfundoNgwenya <Nomfundo Ngwenya@.com>
2023-08-29 01:13:56 -0700

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * pre_main - Prints specified lines before the main function is executed.
+ */
+void pre_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
