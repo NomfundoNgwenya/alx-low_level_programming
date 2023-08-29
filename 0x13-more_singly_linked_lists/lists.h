2c2c38249526d529ce58c7d6dff9a2006bfe0d43
NomfundoNgwenya <Nomfundo Ngwenya@.com>
2023-08-29 05:25:20 -0700

    lasts print list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,6 +2,8 @@
 #define LISTS_H
 
 #include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 
 typedef struct listint_s
@@ -20,6 +22,5 @@ typedef struct listint_s
  */
 
 size_t print_listint(const listint_t *h);
-size_t listint_len(const listint_t *h);
 
-#endif
+#endif /* LISTS_H */
